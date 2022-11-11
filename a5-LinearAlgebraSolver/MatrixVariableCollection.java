cda15dbdc7ce20ecc95917be81dc8088338faa52
Jared Jordan <jordanjared1616@gmail.com>
2022-11-11 11:21:54 -0500

    Update MatrixVariableCollection.java

--- a5-LinearAlgebraSolver/MatrixVariableCollection.java
+++ a5-LinearAlgebraSolver/MatrixVariableCollection.java
@@ -3,7 +3,6 @@
 
 public class MatrixVariableCollection
 {
-
 	private Matrix matrixA;
 	private Matrix matrixB;
 	private Matrix matrixC;
@@ -11,14 +10,12 @@ public class MatrixVariableCollection
 	private Matrix matrixE;
 
 	public MatrixVariableCollection() {
-		
 		//All matricies are initiliazed to size zero until they are declared within the declareMatrixScene
 		matrixA = new Matrix(0, 0);
 		matrixB = new Matrix(0, 0);
 		matrixC = new Matrix(0, 0);
 		matrixD = new Matrix(0, 0);
 		matrixE = new Matrix(0, 0);
-		
 	}
 	
 	/*
@@ -28,34 +25,21 @@ public class MatrixVariableCollection
 		evaluated, it returns the string associated with the matrix name.
 	*/
 	public Matrix getMatrix(String matrixVariableName) {
-		
 		Matrix temporaryMatrix;
 		
 		if(matrixVariableName.equals("A")) {
-			
 			temporaryMatrix = matrixA;
-			
 		}else if(matrixVariableName.equals("B")) {
-			
 			temporaryMatrix = matrixB;
-			
 		}else if(matrixVariableName.equals("C")) {
-			
 			temporaryMatrix = matrixC;
-			
 		}else if(matrixVariableName.equals("D")) {
-			
 			temporaryMatrix = matrixD;
-			
 		}else if(matrixVariableName.equals("E")) {
-			
 			temporaryMatrix = matrixE;
-			
 		}else {
-			
 			System.out.println("sdfasdfsd");
 			temporaryMatrix = null;
-			
 		}
 		
 		return temporaryMatrix;
