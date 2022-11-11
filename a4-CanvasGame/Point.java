8f96ef0148ca8b616f1480742f1238aca028b092
Jared Jordan <jordanjared1616@gmail.com>
2022-11-11 11:06:47 -0500

    Update Point.java

--- a4-CanvasGame/Point.java
+++ a4-CanvasGame/Point.java
@@ -8,9 +8,7 @@ public class Point extends Block {
 	//speed of point will always remain 0.
 	
 	public Point(int xSpeedIn, int ySpeedIn, int sizeIn, Image imageIn) {
-		
 		super(xSpeedIn, ySpeedIn, sizeIn, imageIn);
-		
 	}
 	
 	//The point should be placed away from the player
@@ -22,21 +20,16 @@ public class Point extends Block {
 		
 		//A for loop was used to sacrifice the point possibly hitting the player compared to using a while loop that might be infite
 		for(int i = 0; i < 5; i++) {
-			
 			//Checks to see if the random location overlaps player, if it does, generate new number else setLoc and break
 			if(this.checkCollision((Block)player)) {
-
 				randX = (int)(Math.random()*(CANVAS_WIDTH-getSize()));
 				randY = (int)(Math.random()*(CANVAS_HEIGHT-getSize()));
 				setLoc(randX, randY);
-				
 			}else{
 
 				setLoc(randX, randY);
 				break;
-				
 			}
-			
 		}		
 		
 	}
